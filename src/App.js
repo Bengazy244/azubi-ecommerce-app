3da8ee43328fb9beb602b398256e0f27df44d10f
Bengazy244 <bengazy244gmail.com>
2023-03-23 12:56:06 +0000

    Added product card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import HomePage from "./component/HomePage";
 import SignIn from "./component/SignIn";
 
 function App() {
   return (
     <div className="App">
+      <HomePage />
       <SignIn />
     </div>
   );
