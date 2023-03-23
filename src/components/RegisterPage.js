d46140bf43d392f06ec518e28236e407277534ec
AJAnimah <linmell2010@gmail.com>
2023-03-23 23:08:31 +0000

    created new folders and files

---
+++ src/components/RegisterPage.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+const RegisterPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (password === confirmPassword) {
+      localStorage.setItem("email", email);
+      localStorage.setItem("password", password);
+
+      alert("Account created successfully!");
+
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+    } else {
+      alert("Passwords do not match!");
+
+      setPassword("");
+      setConfirmPassword("");
+    }
+  };
+
+  return (
+    <div className="registration-page">
+      <h1>Create an Account</h1>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email Address:</label>
+
+        <input
+          type="text"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="password">Password:</label>
+
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <label htmlFor="confirm-password">Confirm Password:</label>
+
+        <input
+          type="password"
+          name="confirm-password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+
+        <button type="submit">Create an Account</button>
+      </form>
+    </div>
+  );
+};
+export default RegisterPage;
