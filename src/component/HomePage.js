7b6b21a85d4b27c06647d5674b2048dfc70f792b
Bengazy244 <bengazy244gmail.com>
2023-03-23 12:53:34 +0000

    Created HomePage

---
+++ src/component/HomePage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import product_card from "./product_card";
+import "../App.css";
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Welcome to our Ecommerce Store</h1>
+      <div className="product-list">
+        {product_card.map((product) => (
+          <div className="product-card" key={product.id}>
+            <img
+              src={process.env.PUBLIC_URL + product.thumb}
+              alt={product.product_name}
+            />
+            <h2>{product.product_name}</h2>
+            <p>{product.description}</p>
+            <div className="product-price">
+              <span className="currency">{product.currency}</span>
+              <span className="price">{product.price}</span>
+            </div>
+            <button className="add-to-cart">Place order</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
