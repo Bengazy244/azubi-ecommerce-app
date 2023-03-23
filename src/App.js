cfc935ea7b98ffcea42f1fbebf92b3a131e568a1
Bengazy244 <bengazy244gmail.com>
2023-03-23 21:35:59 +0000

    Added logo and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import HomePage from "./component/HomePage";
+import Navbar from "./component/Navbar";
 import SignIn from "./component/SignIn";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <HomePage />
       <SignIn />
     </div>
